c8e0feb80ccec6a5e3d609d74fdbac84463a4e2f
Mn <fluo@Fluos-iMac.local>
2017-03-31 12:22:53 -0600

    Parallax without tween

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log("Hola")
+var _els = document.getElementsByClassName('parallax'),
+    _layers = [],
+    _scrollY = window.scrollY;
+
+for(var i =0; i < _els.length; i++){
+  var el = _els[i];
+  var offset = _els[i].dataset.offset;
+  _layers.push({
+    el : el,
+    y : 0,
+    offset : offset
+  });
+};
+
+window.addEventListener('scroll', handle_scroll);
+requestAnimationFrame(animate);
+
+function handle_scroll(e){
+    _scrollY = window.scrollY;
+    console.log(_scrollY);
+}
+
+function animate(){
+  for (var i = 0; i < _layers.length; i++) {
+    var oldY = _layers[i].y;
+    _layers[i].y = _scrollY * _layers[i].offset;
+    if(oldY !== _layers[i].y){
+      _layers[i].el.style.transform = 'translateY('+ _layers[i].y +'px)';
+    }
+  }
+  requestAnimationFrame(animate);
+}
